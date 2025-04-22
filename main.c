583dc4fae4dfd9a0e18c72db90b445f21cdf27a7
lkjsxc <lkjsxc@gmail.com>
2025-04-22 07:21:04 +0000

    Refactor memory handling in readsrc function and update data structure for file reading

--- main.c
+++ main.c
@@ -1,14 +1,33 @@
 #include <stdio.h>
+#include <stdint.h>
 
 #define src_path "./src.txt"
-#define src_data_size 65536
-
-char src_data[src_data_size];
+#define mem_size 65536
 
 typedef enum {
     OK,
     NG,
 } status_t;
+
+typedef enum {
+    INST_NULL,
+    INST_PUSH,
+    INST_ASSIGN,
+} inst_t;
+
+typedef struct {
+    inst_t inst;
+    char* token;
+} node_t;
+
+union {
+    int32_t i32[mem_size / sizeof(int32_t)];
+    struct {
+        int32_t bin[mem_size / sizeof(int32_t)];
+        char src[mem_size / sizeof(char)];
+        node_t node[mem_size / sizeof(node_t)];
+    } compile_data;
+} mem;
 
 status_t readsrc() {
     FILE* file = fopen(src_path, "r");
@@ -16,21 +35,24 @@ status_t readsrc() {
         perror("Error opening file");
         return NG;
     }
-    size_t bytes_read = fread(src_data, 1, src_data_size - 1, file);
+    size_t bytes_read = fread(mem.compile_data.src, sizeof(char), mem_size, file);
     if (bytes_read == 0) {
         perror("Error reading file");
         fclose(file);
         return NG;
     }
-    src_data[bytes_read] = '\n';
-    src_data[bytes_read + 1] = '\0';
+    mem.compile_data.src[bytes_read] = '\n';
+    mem.compile_data.src[bytes_read + 1] = '\0';
     fclose(file);
+}
+
+status_t parse() {
 }
 
 int main() {
     if (readsrc() == NG) {
         return 1;
     }
-    printf("Data read from file:\n%s", src_data);
+    printf("Data read from file:\n%s", mem.compile_data.src);
     return 0;
 }
