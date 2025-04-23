2fcd8be62000208eac43552302e56ae007980e94
lkjsxc <lkjsxc@gmail.com>
2025-04-23 04:29:07 +0000

    Implement loop structure in example source and update parsing logic for label handling

--- main.c
+++ main.c
@@ -164,6 +164,14 @@ void parse_primary(int label_break, int label_continue) {
         } else {
             *(mem.compile_data.node_itr++) = (node_t){.inst = LABEL, .token = NULL, .val = label_if};
         }
+    } else if (token_eq(mem.compile_data.src_itr, "loop")) {
+        int label_start = mem.compile_data.label_cnt++;
+        int label_end = mem.compile_data.label_cnt++;
+        mem.compile_data.src_itr = token_next(mem.compile_data.src_itr);
+        *(mem.compile_data.node_itr++) = (node_t){.inst = LABEL, .token = NULL, .val = label_start};
+        parse_expr(label_break, label_continue);
+        *(mem.compile_data.node_itr++) = (node_t){.inst = INST_JMP, .token = NULL, .val = label_start};
+        *(mem.compile_data.node_itr++) = (node_t){.inst = LABEL, .token = NULL, .val = label_end};
     } else if (token_eq(mem.compile_data.src_itr, "return")) {
         mem.compile_data.src_itr = token_next(mem.compile_data.src_itr);
         parse_expr(label_break, label_continue);
